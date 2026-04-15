3a50e078811ca8b5a307c7f8ce1936c2a3b4c1ec
defoqofficial <118861330+defoqofficial@users.noreply.github.com>
2026-04-15 14:19:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "StrandKit | The Hair, Fur & Dynamics Library",
     "author": "Nino Defoq",
-    "version": (1, 0, 6),
+    "version": (1, 0, 7),
     "blender": (4, 2, 0),
     "location": "View3D > Sidebar > StrandKit",
     "description": "Switches hair card textures based on folder structure and bakes maps.",
