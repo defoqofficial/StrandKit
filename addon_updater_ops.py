eb83f5b2e70e58017344fae1dd2c29cedc7bda6c
defoqofficial <118861330+defoqofficial@users.noreply.github.com>
2026-06-29 10:18:50 +0200

    SSL update

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -32,6 +32,14 @@ from bpy.props import StringProperty, FloatProperty
 import gpu
 from gpu.types import GPUVertFormat, GPUVertBuf
 from gpu.shader import from_builtin
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 # Safely import the updater.
 try:
